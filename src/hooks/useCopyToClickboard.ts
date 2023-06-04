99f87cd33175a15f5b74d45dc9e8e84fff9370f7
nacukisubaru <incloudff@gmail.com>
2023-06-04 17:02:07 +0300

    feat add copy function for linux

--- src/hooks/useCopyToClickboard.ts
+++ src/hooks/useCopyToClickboard.ts
@@ -8,8 +8,23 @@ function useCopyToClipboard(): [CopyFn, CopiedValue] {
 
   const copy: CopyFn = async text => {
     if (!navigator?.clipboard) {
-      console.warn('Clipboard not supported')
-      return false
+      const textArea = document.createElement("textarea");
+      textArea.value = text;
+          
+      // Move textarea out of the viewport so it's not visible
+      textArea.style.position = "absolute";
+      textArea.style.left = "-999999px";
+          
+      document.body.prepend(textArea);
+      textArea.select();
+
+      try {
+          document.execCommand('copy');
+      } catch (error) {
+          console.error(error);
+      } finally {
+          textArea.remove();
+      }
     }
 
     // Try to save to clipboard then save it in the state if worked
